d30129b58cae35e7101488ccd875deea4662ead9
ahmringholm <ringholmahm4@gmail.com>
2025-11-26 09:19:03 +0100

    added import to main.js, added person 1 2 3 4, adjusted boolean in Person.js

--- Person.js
+++ Person.js
@@ -1,11 +1,30 @@
+// export class Person {
+//     constructor() {
+//         this.firstName = "Ahm";
+//         this.lastName = "Ringholm";
+//         this.age = 29;
+//         this.isActive = true;
+//         this.hobbies = ["programmering", "grafisk design", "film"];
+//         this.meta = { role: "student", country: "Sverige" }
+//     }
+
+//     getFullName() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+
+//     getInfo() {
+//         return `${this.getFullName()} är ${this.age} år och är en ${this.meta.role} från ${this.meta.country}.`;
+//     }
+// }
+
 export class Person {
-    constructor() {
-        this.firstName = "Ahm";
-        this.lastName = "Ringholm";
-        this.age = 29;
-        this.isActive = true;
-        this.hobbies = ["programmering", "grafisk design", "film"];
-        this.meta = { role: "student", country: "Sverige" }
+    constructor(firstName, lastName, age, isActive, hobbies, meta) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.isActive = isActive;
+        this.hobbies = hobbies;
+        this.meta = meta;
     }
 
     getFullName() {
@@ -13,6 +32,10 @@ export class Person {
     }
 
     getInfo() {
-        return `${this.getFullName()} är ${this.age} år och är en ${this.meta.role} från ${this.meta.country}.`;
+        return `${this.firstName} ${this.lastName} är ${this.age} år och är en ${this.meta.role} i ${this.meta.country}.` + ` Aktiv: ${this.isActive ? "Ja" : "Nej"}.`;
+    }
+
+    toggleActive() {
+        this.isActive = !this.isActive;
     }
 }
--- main.js
+++ main.js
@@ -1,6 +1,82 @@
+// import { Person } from "./Person.js";
+
+// const person = new Person();
+
+// console.log(person.getFullName());
+// console.log(person.getInfo());
+
+// import { Person } from "./Person.js";
+
+// const person = new Person(
+//     "Ahm",
+//     "Ringholm",
+//     29,
+//     true,
+//     ["programmering", "grafisk design", "film"],
+//     { role: "student", country: "Sverige" }
+// );
+
+// console.log(person.getFullName());
+// console.log(person.getInfo());
+
+// person.toggleActive();
+// console.log(person.isActive);
+
 import { Person } from "./Person.js";
 
-const person = new Person();
+const person1 = new Person(
+    "Ahm",
+    "Ringholm",
+    29,
+    true,
+    ["programmering", "grafisk design", "film"],
+    { role: "student", country: "Sverige" }
+);
 
-console.log(person.getFullName());
-console.log(person.getInfo());
+const person2 = new Person(
+    "Kent",
+    "Andersson",
+    67,
+    false,
+    ["blommor", "laga mat"],
+    { role: "pensionär", country: "Sverige" }
+);
+
+const person3 = new Person(
+    "Birgitta",
+    "Malm",
+    45,
+    false,
+    ["böcker", "nyheter", "måla"],
+    { role: "bildlärare", country: "Sverige" }
+);
+
+const person4 = new Person(
+    "Emma",
+    "Jonsson",
+    16,
+    true,
+    ["fotboll", "matte"],
+    { role: "gymnasieelev", country: "Sverige" }
+);
+
+const people = [person1, person2, person3, person4];
+
+console.log(person1.getInfo());
+console.log(person2.getInfo());
+console.log(person3.getInfo());
+console.log(person4.getInfo());
+
+const output = document.querySelector("#output");
+
+person1.toggleActive();
+console.log(person1.isActive);
+
+person2.toggleActive();
+console.log(person2.isActive);
+
+person3.toggleActive();
+console.log(person3.isActive);
+
+person4.toggleActive();
+console.log(person4.isActive);
