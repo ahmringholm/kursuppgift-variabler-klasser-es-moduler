1e80c976126f8f1ec168233343adfdf2406be3bd
ahmringholm <ringholmahm4@gmail.com>
2025-11-26 08:43:19 +0100

    added class for Person

---
+++ Person.js
@@ -0,0 +1,18 @@
+export class Person {
+    constructor() {
+        this.firstName = "Ahm";
+        this.lastName = "Ringholm";
+        this.age = 29;
+        this.isActive = true;
+        this.hobbies = ["programmering", "grafisk design", "film"];
+        this.meta = { role: "student", country: "Sweden" }
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    getInfo() {
+        return `${this.getFullName()} är ${this.age} år och är en ${this.meta.role} från ${this.meta.country}.`;
+    }
+}
